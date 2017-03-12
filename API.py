ee2848ed60ae6da21f929288dbfc3245924c0dcc
Alejandro <alejandrombc@gmail.com>
2017-03-11 23:05:04 -0400

    Updatear y agregar producto

--- API.py
+++ API.py
@@ -23,6 +23,11 @@ errors = {
     },
     'ProductoNotFound': {
         'message': "El producto seleccionado no existe",
+        'status': 404,
+        'product_value': False,
+    },
+    'ProductoYaCreado': {
+        'message': "El producto que sea ingresar ya esta creado",
         'status': 410,
         'product_value': False,
     }
@@ -40,6 +45,16 @@ success = {
     	'access_token' : True,
     	'status' : 200,
     	'login_value' : True
+    },
+    'ProductoAgregado':{
+    	'message': "Producto agregado exitosamente",
+    	'status' : 200,
+    	'product_value' : True
+    },
+    'ProductoEditado':{
+    	'message': "Producto editado exitosamente",
+    	'status' : 200,
+    	'product_value' : True
     }
 }
 
@@ -68,13 +83,8 @@ class Productos(Resource):
 
 		if(producto != None): return dict(id=producto[0], nombre=producto[1], descripcion=producto[2], foto=producto[3], precio=producto[4], cantVendida=producto[5], idCategoria=producto[6])
 
-		return errors['ProductoNotFound']
+		return errors['ProductoNotFound'], 404
 
-	def put(self, idP):
-		productos[idP] = request.form['data']
-		productos['cantidad'] = request.form['cant']
-		productos['prod_ep'] = request.form['prod_ep']
-		return {idP: productos[idP], "cantidad" : productos['cantidad'], "prod_ep" : productos['prod_ep']}
 
 class ProductosList(Resource):
 	def get(self):
@@ -86,6 +96,47 @@ class ProductosList(Resource):
 
 		return ([dict(id=producto[0], nombre=producto[1], descripcion=producto[2], foto=producto[3], precio=producto[4], cantVendida=producto[5], idCategoria=producto[6]) for producto in data])
 
+	def post(self):
+		producto = request.get_json()
+
+		con = mysql.connect()
+		cursor = con.cursor()
+
+		cursor.execute("SELECT idProducto from producto WHERE idProducto=%s", (producto['idProducto']))
+		hay_producto = cursor.fetchone()
+
+		if(hay_producto != None): return errors['ProductoYaCreado'], 410
+
+		cursor.execute("INSERT INTO producto VALUES (%s,%s,%s,%s,%s,%s,%s)", (producto['idProducto'], producto['nombre'], producto['descripcion'], producto['foto'], producto['precio'], producto['cantVendida'], producto['idCategoria']))
+		con.commit()
+
+		return success['ProductoAgregado'], 200
+
+	def put(self):
+		producto = request.get_json()
+
+		con = mysql.connect()
+		cursor = con.cursor()
+
+		cursor.execute("SELECT * from producto WHERE idProducto=%s", (producto['idProducto']))
+		hay_producto = cursor.fetchone()
+
+		if(hay_producto == None): return errors['ProductoNotFound'], 410
+
+
+		if(producto['idProducto'] == None): producto['idProducto'] = hay_producto[0]
+		if(producto['nombre'] == None): producto['nombre'] = hay_producto[1]
+		if(producto['descripcion'] == None): producto['descripcion'] = hay_producto[2]
+		if(producto['foto'] == None): producto['foto'] = hay_producto[3]
+		if(producto['precio'] == None): producto['precio'] = hay_producto[4]	
+		if(producto['cantVendida'] == None): producto['cantVendida'] = hay_producto[5]	
+		if(producto['idCategoria'] == None): producto['idCategoria'] = hay_producto[6]	
+
+
+		cursor.execute("UPDATE producto SET idProducto=%s, nombre=%s, descripcion=%s, foto=%s, precio=%s, cantVendida=%s, idCategoria=%s WHERE idProducto=%s", (producto['idProducto'], producto['nombre'], producto['descripcion'], producto['foto'], producto['precio'], producto['cantVendida'], producto['idCategoria'], producto['idProducto']))
+		con.commit()
+
+		return success['ProductoEditado'], 200
 
 
 class Register(Resource):
